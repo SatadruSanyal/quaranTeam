0f12198d19ea3a8b9da44e51725b951b324017a3
Russell <russelltangwl@gmail.com>
2020-05-31 02:31:19 +0800

    Categories page implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'selectInterests.dart';
+String categoryFocus = "";
 
 void main() => runApp(MyApp());
 
@@ -13,6 +15,98 @@ class MyApp extends StatelessWidget {
       ),
       home: Interests());
   }
+}
+
+
+class CategoriesState extends State<Interests>{
+  final _suggestions = <Text>[];
+  final _biggerFont = const TextStyle(fontSize: 15.0);
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select a category'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list)),
+        ],
+      ),
+      body: _startColumn(),
+    );
+  }
+
+  Widget _startColumn() {
+    return Column(
+      children: <Widget>[
+        Text('Please select a category', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        Expanded(
+            child: _buildSuggestions()
+        ),
+        FlatButton(
+          color: Colors.green,
+          textColor: Colors.blueGrey,
+          //onPressed: _homePage,
+          child: Text(
+            "Done",
+            style: TextStyle(fontSize: 30.0),
+          ),
+        )
+//        IconButton(
+//          icon: Icon(Icons.check_circle, color: Colors.green,),
+//          iconSize: 40.0,
+//          onPressed: _homePage
+//        )
+      ],
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          _addSuggestions();
+          if (i < 5) {
+            return _buildRow(_suggestions[i]);
+          } else {
+            return null;
+          }
+        }
+
+    );
+  }
+
+  void _addSuggestions() {
+    //Hardcoded suggestions can be added here
+    _suggestions.add(Text('Sports'));
+    _suggestions.add(Text('Entertainment'));
+    _suggestions.add(Text('Health and fitness'));
+    _suggestions.add(Text('Arts'));
+    _suggestions.add(Text('Technology'));
+
+  }
+
+  Widget _buildRow(Text word) {
+    return ListTile(
+      title: Text(
+        word.data,
+        style: _biggerFont,
+      ),
+      onTap: () {
+        //Find which category was tapped
+        categoryFocus = word.data;
+        //Move over to InterestsState
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => InterestsState().build(context)),
+        );
+        //EOF
+      },
+    );
+  }
+
+
+
+  //End of class!
 }
 
 class InterestsState extends State<Interests> {
@@ -36,7 +130,7 @@ class InterestsState extends State<Interests> {
     Navigator.of(context).push(MaterialPageRoute<void>(
       builder: (BuildContext context) {
         final Iterable<ListTile> tiles = _saved.map(
-          (Text word) {
+              (Text word) {
             return ListTile(
               title: Text(
                 word.data,
@@ -62,14 +156,18 @@ class InterestsState extends State<Interests> {
   Widget _startColumn() {
     return Column(
       children: <Widget>[
-        Text('Welcome! Please Choose Your Interests', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        Text('Please Choose Your Interests', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
         Expanded(
             child: _buildSuggestions()
         ),
-        IconButton(
-          icon: Icon(Icons.check_circle, color: Colors.green,),
-          iconSize: 40.0,
-          onPressed: _homePage
+        FlatButton(
+          color: Colors.green,
+          textColor: Colors.blueGrey,
+          //onPressed: _homePage,
+          child: Text(
+            "Done",
+            style: TextStyle(fontSize: 30.0),
+          ),
         )
       ],
     );
@@ -77,14 +175,14 @@ class InterestsState extends State<Interests> {
 
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        _addSuggestions();
-        if (i < 3) {
-          return _buildRow(_suggestions[i]);
-        } else {
-          return null;
-        }
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          _addSuggestions();
+          if (i < 5) {
+            return _buildRow(_suggestions[i]);
+          } else {
+            return null;
+          }
         }
 
     );
@@ -129,9 +227,41 @@ class InterestsState extends State<Interests> {
 
   void _addSuggestions() {
     //Hardcoded suggestions can be added here
-    _suggestions.add(Text('Painting'));
-    _suggestions.add(Text('Video Games'));
-    _suggestions.add(Text('Religion'));
+    if (categoryFocus == "Sports"){
+      _suggestions.add(Text('Football'));
+      _suggestions.add(Text('Golf'));
+      _suggestions.add(Text('Tennis'));
+      _suggestions.add(Text('Basketball'));
+      _suggestions.add(Text('Motorsports'));
+    }
+    else if (categoryFocus == "Entertainment"){
+      _suggestions.add(Text('Movies'));
+      _suggestions.add(Text('New media'));
+      _suggestions.add(Text('Music Videos'));
+      _suggestions.add(Text('Fashion Shows'));
+      _suggestions.add(Text('Documentaries'));
+    }
+    else if (categoryFocus == "Health and fitness"){
+      _suggestions.add(Text('Running'));
+      _suggestions.add(Text('Yoga'));
+      _suggestions.add(Text('Pilates'));
+      _suggestions.add(Text('Weightlifting'));
+      _suggestions.add(Text('Calisthenics'));
+    }
+    else if (categoryFocus == "Arts"){
+      _suggestions.add(Text('Fine art'));
+      _suggestions.add(Text('Contemporary'));
+      _suggestions.add(Text('Art Nouveau'));
+      _suggestions.add(Text('Pop art'));
+      _suggestions.add(Text('Minimalism Art'));
+    }
+    else if (categoryFocus == "Technology"){
+      _suggestions.add(Text('Phones'));
+      _suggestions.add(Text('PC building'));
+      _suggestions.add(Text('Gaming'));
+      _suggestions.add(Text('Crypto'));
+      _suggestions.add(Text('DIYs'));
+    }
   }
 
   void _homePage() {
@@ -153,5 +283,5 @@ class InterestsState extends State<Interests> {
 
 class Interests extends StatefulWidget {
   @override
-  InterestsState createState() => InterestsState();
+  CategoriesState createState() => CategoriesState();
 }
