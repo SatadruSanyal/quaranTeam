ac62555a31ec5fbfddb39f6a2eeb9edcc9f0c9ea
Satadru Sanyal <satadru8668@gmail.com>
2020-06-01 03:00:39 +0400

    skeleton code for calendar commented out

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'bottomNavigationBar.dart';
+//import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CalendarState();
+}
+
+final TextStyle headerFont = TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+
+class CalendarState extends State<Calendar> {
   BuildContext thisContext;
+
+//  CalendarController _controller;
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    _controller = CalendarController();
+//  }
+//
+//  @override
+//  void dispose() {
+//    _controller.dispose();
+//    super.dispose();
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +32,13 @@ class Calendar extends StatelessWidget {
       appBar: AppBar(
         title: Text('Calendar'),
       ),
-      body: Center(
-        child: Text('Coming Soon!', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-      ),
+      body: Center(child: Text('Coming Soon', style: headerFont)), //Column(
+//        children: <Widget>[
+//          Container(
+//            child: TableCalendar(calendarController: _controller),
+//          )
+//        ],
+//      ),
       bottomNavigationBar: bottomNavBar(4),
     );
   }
