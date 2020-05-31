232faa1b588f1ef741b5be0a9b0c7bf087d19ffb
Satadru Sanyal <satadru8668@gmail.com>
2020-05-31 05:11:19 +0400

    Added Textfields and save button on new journal entry - save currently does nothing

--- lib/newJournalEntry.dart
+++ lib/newJournalEntry.dart
@@ -6,7 +6,7 @@ final TextStyle headerFont = TextStyle(fontSize: 24, fontWeight: FontWeight.bold
 Widget entry;
 
 class JournalEntry extends StatelessWidget {
-  String category = 'Select category';
+  String category = 'Select Category';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +35,33 @@ class JournalEntry extends StatelessWidget {
         ),
             Row(
               children: <Widget>[
+                Text('Event Category:', style: standardFont,),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: DropdownButton<String>(
+                    value: category,
+                    icon: Icon(Icons.arrow_drop_down),
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                      color: Colors.black
+                    ),
+                    onChanged: (String newValue) {
+                      category = newValue;
+                    },
+                    items: <String>['Select Category','Sport', 'Technology', 'Entertainment', 'Arts', 'Health and Fitness']
+                    .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value)
+                      );
+                    }).toList(),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
                 Text('Event Description:', style: standardFont),
                 Expanded(
                 child: Padding(
@@ -49,6 +76,30 @@ class JournalEntry extends StatelessWidget {
                 )
 
               ],
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: 111,
+                  height: 50,
+                  child: RaisedButton(
+                    onPressed: saveEvent,
+                    color: Colors.green,
+                    textColor: Colors.white,
+
+                    child: Row(
+                      children: <Widget>[
+                        Text('Save', style: standardFont),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.check, color: Colors.white,),
+                        )
+                      ],
+                    )
+                  ),
+                ),
+              ),
             )
           ],
         ),
@@ -56,4 +107,7 @@ class JournalEntry extends StatelessWidget {
     );
   }
 
+  void saveEvent() {
+    //TODO: Add event saving functionality
+  }
 }
